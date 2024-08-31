d37bd62ed515a38dd7f604efb5b1c18aa103efa0
hctomkins <hugh3000@googlemail.com>
2024-08-31 21:19:42 +0100

    fix test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,11 +1,11 @@
 from monohull_dynamics.demo.demo import (
-    step_uncontrolled,
-    integrate_many,
     init_simulation_state,
 )
 import jax.numpy as jnp
 from jax import jit
 import time
+
+from monohull_dynamics.dynamics.boat import integrate_many, step_uncontrolled
 
 
 def test_e2e():
@@ -15,19 +15,19 @@ def test_e2e():
     N = 10
     t0 = time.time()
     for i in range(N):
-        state = step_uncontrolled(state, dt)
+        new_boat_state = step_uncontrolled(state.boat_state, state.force_model, state.wind_velocity, dt)
     print(f"Time taken per un-jitted step: {(time.time()-t0)/N}")
 
     N = 1000
     j_step_uncontrolled = jit(step_uncontrolled)
     t0 = time.time()
     for i in range(N):
-        state = j_step_uncontrolled(state, dt)
+        new_boat_state = j_step_uncontrolled(state.boat_state, state.force_model, state.wind_velocity, dt)
     print(f"Time taken per jitted step: {(time.time()-t0)/N}")
 
     N = 1000000
     j_many = jit(integrate_many)
-    j_many(state, dt, N)
+    j_many(state.boat_state, state.force_model, state.wind_velocity, dt, N)
     t0 = time.time()
-    state = j_many(state, dt, N)
+    _ = j_many(state.boat_state, state.force_model, state.wind_velocity, dt, N)
     print(f"Time taken per step in jitted loop: {(time.time()-t0)/N}")
