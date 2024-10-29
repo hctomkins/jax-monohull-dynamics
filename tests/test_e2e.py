3765b1e2ac1cba5a55324ecfca02c731a7307f91
Hugh Tomkins <hugh3000@googlemail.com>
2024-10-29 10:48:24 +0000

    update e2e test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -5,37 +5,41 @@ from monohull_dynamics.dynamics.boat import j_integrate_many, j_step_uncontrolle
 import time
 import jax.profiler
 
+from monohull_dynamics.dynamics.wind import evaluate_wind
+
 
 def test_e2e():
     rng = jax.random.PRNGKey(0)
     sim_state = init_simulation_state(rng=rng)
     boat_state = sim_state.boat_state
+    boat_state = jax.tree.map(lambda x: x[0], boat_state)
+    wind_velocity = evaluate_wind(sim_state.wind_state, boat_state.particle_state.x)
 
     dt = 0.1
 
     N = 10
     t0 = time.time()
     for i in range(N):
-        state = step_uncontrolled(boat_state, sim_state.force_model, sim_state.wind_velocity, dt)
+        state = step_uncontrolled(boat_state, sim_state.force_model, wind_velocity, dt)
     print(f"Time taken per un-jitted step: {(time.time()-t0)/N}")
 
     N = 1000
     t0 = time.time()
-    j_step_uncontrolled(boat_state, sim_state.force_model, sim_state.wind_velocity, dt)
+    j_step_uncontrolled(boat_state, sim_state.force_model, wind_velocity, dt)
     for i in range(N):
-        state = j_step_uncontrolled(boat_state, sim_state.force_model, sim_state.wind_velocity, dt)
+        state = j_step_uncontrolled(boat_state, sim_state.force_model, wind_velocity, dt)
     print(f"Time taken per jitted step: {(time.time()-t0)/N}")
 
     N = 10000
-    j_integrate_many(boat_state, sim_state.force_model, sim_state.wind_velocity, dt, N)
+    j_integrate_many(boat_state, sim_state.force_model, wind_velocity, dt, N)
     t0 = time.time()
-    state = j_integrate_many(boat_state, sim_state.force_model, sim_state.wind_velocity, dt, N)
+    state = j_integrate_many(boat_state, sim_state.force_model, wind_velocity, dt, N)
     print(f"Time taken per step in jitted loop: {(time.time()-t0)/N}")
 
-    print("tracing")
-    with jax.profiler.trace("jax-trace-step-annotate-interp"):
-        state = j_integrate_many(boat_state, sim_state.force_model, sim_state.wind_velocity, dt, N)
-        print("done")
+    # print("tracing")
+    # with jax.profiler.trace("jax-trace-step-annotate-interp"):
+    #     state = j_integrate_many(boat_state, sim_state.force_model, wind_velocity, dt, N)
+    #     print("done")
 
 
 if __name__ == "__main__":
